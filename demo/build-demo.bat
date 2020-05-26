81a506d30fa0fe23755f34b1e5923cd98af6f954
zhaoyanan <zhaoyanan@indemind.cn>
2020-05-26 19:18:28 +0800

    1. add indemind(opencv331);2. Modify directory name;3. perfect README.md
    Signed-off-by: zhaoyanan <zhaoyanan@indemind.cn>

--- demo/build-demo.bat
+++ demo/build-demo.bat
@@ -16,8 +16,15 @@ echo start... "call cmake ."
 call cmake ..\.
 echo.
 
+echo %cd%
+
 echo .\indemind_demos.sln
 start .\indemind_demos.sln
+
+cd ..
+copy %cd%\..\src\detector\lib\win10-x64\MNN.dll %cd%\output\bin\MNN.dll
+copy %cd%\..\lib\win10-x64\indemind.dll %cd%\output\bin\indemind.dll
+copy %cd%\..\src\detector\config\blazeface.mnn %cd%\output\bin\blazeface.mnn
 
 ::pause
 exit
